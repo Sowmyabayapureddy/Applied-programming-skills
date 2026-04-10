0e9d90e6fad2b31ba5b140a1ce71ea512faec913
Sowmyabayapureddy <sowmyabayapureddy@gmail.com>
2026-04-10 11:01:49 +0530

    Time: 1 ms (24.16%) | Memory: 43.8 MB (15.40%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,20 +1,21 @@
 class Solution {
-  public void sortColors(int[] nums) {
-    int l = 0;
-    int r = nums.length - 1;
+    public void sortColors(int[] nums) {
+        HashMap<Integer, Integer> count = new HashMap<>();
+        count.put(0, 0);
+        count.put(1, 0);
+        count.put(2, 0);
 
-    for (int i = 0; i <= r;)
-      if (nums[i] == 0)
-        swap(nums, i++, l++);
-      else if (nums[i] == 1)
-        ++i;
-      else
-        swap(nums, i, r--);
-  }
+        for (int num : nums) {
+            count.put(num, count.get(num) + 1);
+        }
 
-  private void swap(int[] nums, int i, int j) {
-    final int temp = nums[i];
-    nums[i] = nums[j];
-    nums[j] = temp;
-  }
+        int idx = 0;
+        for (int color = 0; color < 3; color++) {
+            int freq = count.get(color);
+            for (int j = 0; j < freq; j++) {
+                nums[idx] = color;
+                idx++;
+            }
+        }        
+    }
 }
