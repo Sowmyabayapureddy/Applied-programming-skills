6a89ce450608fcf2e79b4032f806a0db49cf3165
Sowmyabayapureddy <sowmyabayapureddy@gmail.com>
2026-04-10 11:04:53 +0530

    Time: 5 ms (15.70%) | Memory: 43.4 MB (31.18%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -5,14 +5,17 @@ class Solution {
         mapping.put(')', '(');
         mapping.put('}', '{');
         mapping.put(']', '[');
-        for(char c : s.toCharArray()){if(mapping.containsValue(c)){
-            stack.push(c);
-        }else if(mapping.containsKey(c)){if(stack.isEmpty() || mapping.get(c) != stack.pop()){
-            return false;
+
+        for (char c : s.toCharArray()) {
+            if (mapping.containsValue(c)) {
+                stack.push(c);
+            } else if (mapping.containsKey(c)) {
+                if (stack.isEmpty() || mapping.get(c) != stack.pop()) {
+                    return false;
+                }
+            }
         }
-           
-        }}
-        return stack.isEmpty();
-        
+
+        return stack.isEmpty();        
     }
 }
